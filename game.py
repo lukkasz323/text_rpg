b567d496aba2a0a14818ffc06fa4062de28b7651
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-06-29 23:50:38 +0200

    Add intro and basic functionality

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 def main():
-    pass
+    # Intro
+    print(['TEXT RPG'])
+    input("Press Enter to start a new game...")
+    
+    # Hub
+    print('[HUB]\n1. Fight\n2. Shop')
 
 if __name__ == '__main__':
     main()
