bf3f1738203f4f7c4fc3f8048cdc53088330a469
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-06-30 00:36:51 +0200

    Basic stats, fight, shop

--- game.py
+++ game.py
@@ -1,10 +1,30 @@
+import os
+
+class Player:
+    def __init__(self):
+        self.name = 'Unknown'
+        self.lvl = 1
+        self.xp = 0
+
 def main():
-    # Intro
-    print(['TEXT RPG'])
-    input("Press Enter to start a new game...")
+    plr = Player()
     
-    # Hub
-    print('[HUB]\n1. Fight\n2. Shop')
+    while True:
+        print(f'[TEXT RPG] - [{plr.name}, LVL: {plr.lvl}, XP: {plr.xp}]\n')
+        print('[HUB]\n(1) Fight\n(2) Shop')
+        inp = input('> ')
+        os.system('cls')
+        match inp:
+            case '1':
+                print('[FIGHT]\n')
+                inp = input('> ')
+            case '2':
+                print('[SHOP]\n')
+                inp = input('> ')
+        os.system('cls')
+    
+    print('The End')
+    
 
 if __name__ == '__main__':
     main()
