2c3ac21646f55e669444aa8796f60972b33a527d
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-06-30 05:19:31 +0200

    Add level ups

--- game.py
+++ game.py
@@ -10,8 +10,14 @@ class Entity:
         
     def attack(self, target):
         dmg = self.str
-        target.hp -= dmg
+        target.hp_sub(dmg)
         return dmg
+    
+    def hp_add(self, amount):
+        self.hp += amount
+        
+    def hp_sub(self, amount):
+        self.hp -= amount
         
 class Player(Entity):
     def __init__(self):
@@ -19,13 +25,23 @@ class Player(Entity):
         self.name = 'Player'
         self.hp = 10
         self.str = 1
-        
         self.lvl = 1
         self.xp = 0
         self.gold = 0
+        self.xp_table = {1: 10, 2: 25, 3: 50, 4: 100, 5: 250}
+        
+    def lvl_up(self):
+        self.xp -= self.xp_table[self.lvl]
+        self.lvl += 1
+        
+    def is_lvl_up_available(self):
+        if self.xp >= self.xp_table[self.lvl]:
+            return True
+        else:
+            return False
         
     def get_status(self):
-        return f'[{self.name}] - [HP: {self.hp}, STR: {self.str}, LVL: {self.lvl}, XP: {self.xp}]\n'
+        return f'[{self.name}] - [HP: {self.hp}, STR: {self.str}] - [LVL: {self.lvl}, XP: {self.xp}] - [GOLD: {self.gold}]\n'
 
 class Enemy(Entity):
     def __init__(self):
@@ -33,9 +49,11 @@ class Enemy(Entity):
         self.name = 'Enemy'
         self.hp = 1
         self.str = 1
+        self.reward_xp = 1
+        self.reward_gold = 1
         
     def get_status(self):
-        return f'[FIGHT] - [{self.name}, HP: {self.hp}]\n'
+        return f'[{self.name}] - [HP: {self.hp}]\n'
         
 class Rat(Enemy):
     def __init__(self):
@@ -43,6 +61,8 @@ class Rat(Enemy):
         self.name = 'Rat'
         self.hp = 4
         self.str = 1
+        self.reward_xp = 2
+        self.reward_gold = 0
         
 class Slime(Enemy):
     def __init__(self):
@@ -50,6 +70,8 @@ class Slime(Enemy):
         self.name = 'Slime'
         self.hp = 6
         self.str = 1
+        self.reward_xp = 3
+        self.reward_gold = 1
         
 class Goblin(Enemy):
     def __init__(self):
@@ -57,6 +79,8 @@ class Goblin(Enemy):
         self.name = 'Goblin'
         self.hp = 8
         self.str = 2
+        self.reward_xp = 5
+        self.reward_gold = 3
 
 def random_enemy(tier=None):
     enemies = [Rat, Slime, Goblin]
@@ -84,7 +108,7 @@ def main():
         os.system('cls')
         match inp:
             case '1':
-                enemy = random_enemy(2)
+                enemy = random_enemy()
                 is_player_turn = True
                 while True:
                     os.system('cls')
@@ -97,8 +121,8 @@ def main():
                                 inp = input('> ')
                                 match inp:
                                     case '1':
-                                        msg(f'{plr.name} attacks.')
-                                        plr.attack(enemy)
+                                        dmg = plr.attack(enemy)
+                                        msg(f'{plr.name} attacks for {dmg}.')
                                     case '2':
                                         msg('Fleeing!', 1)
                                         break
@@ -106,23 +130,35 @@ def main():
                                         msg('You do nothing.')
                                 is_player_turn = False
                             else:
-                                msg(f'{enemy.name} attacks.')
-                                enemy.attack(plr)
+                                dmg = enemy.attack(plr)
+                                msg(f'{enemy.name} attacks for {dmg}.')
                                 is_player_turn = True
                         else:
-                            msg('Enemy defeated!', 1)
-                            # TODO: Reward (Gold, XP)
+                            msg(f'Enemy defeated!\nReward: {enemy.reward_xp} XP | {enemy.reward_gold} Gold', 1)
+                            plr.xp += enemy.reward_xp
+                            plr.gold += enemy.reward_gold
+                            try:
+                                if plr.is_lvl_up_available():
+                                    plr.lvl_up()
+                                    msg('LEVEL UP!')
+                            except KeyError:
+                                break
                             break
                     else:
                         break
                 if plr.hp <= 0:
+                    msg("You've died.", 1)
                     break
             case '2':
+                print('[REST]\n')
+                msg('Resting...', 3)
+                plr.hp_add(5)
+            case '3':
                 print('[SHOP]\n')
                 inp = input('> ')
         os.system('cls')
     os.system('cls')
-    print(f"\n    {plr.name}'s adventure is over.\n\n           The End\n")
+    print(f"\n    {plr.name}'s adventure is over.\n\n             The End\n")
 
 if __name__ == '__main__':
     main()
