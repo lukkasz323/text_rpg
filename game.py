740eaf49cce7f2abecf32a051067b84444883252
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-06-30 03:17:03 +0200

    Implement entity classes and improve fight

--- game.py
+++ game.py
@@ -1,29 +1,99 @@
 import os
+import time
 
-class Player:
+class Entity:
     def __init__(self):
-        self.name = 'Unknown'
+        self.name = 'Entity'
+        self.hp = 1
+        self.str = 1
+        
+    def attack(self, target):
+        dmg = self.str
+        target.hp -= dmg
+        return dmg
+        
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Player'
+        self.hp = 10
+        self.str = 1
         self.lvl = 1
         self.xp = 0
+        self.gold = 0
+        
+    def get_status(self):
+        return f'[{self.name}] - [HP: {self.hp}, STR: {self.str}, LVL: {self.lvl}, XP: {self.xp}]\n'
+
+class Enemy(Entity):
+    def __init__(self):
+        self.name = 'Enemy'
+        self.hp = 1
+        self.str = 1
+        
+    def get_status(self):
+        return f'[FIGHT] - [{self.name}, HP: {self.hp}]\n'
+        
+class Rat(Enemy):
+    def __init__(self):
+        self.name = 'Rat'
+        self.hp = 5
+        self.str = 1
+
+def msg(string, duration=0.5):
+    print(f'\n{string}')
+    time.sleep(duration)
 
 def main():
     plr = Player()
-    
+
     while True:
-        print(f'[TEXT RPG] - [{plr.name}, LVL: {plr.lvl}, XP: {plr.xp}]\n')
+        print(plr.get_status())
         print('[HUB]\n(1) Fight\n(2) Shop')
         inp = input('> ')
         os.system('cls')
         match inp:
             case '1':
-                print('[FIGHT]\n')
-                inp = input('> ')
+                enemy = Rat()
+                is_player_turn = True
+                
+                while True:
+                    os.system('cls')
+                    print(plr.get_status())
+                    print(enemy.get_status())
+                    if plr.hp > 0:
+                        if enemy.hp > 0:
+                            if is_player_turn == True:
+                                print('Choose action: (1) Attack | (2) Flee')
+                                inp = input('> ')
+                                match inp:
+                                    case '1':
+                                        msg(f'{plr.name} attacks.')
+                                        plr.attack(enemy)
+                                    case '2':
+                                        msg('Fleeing!', 1)
+                                        break
+                                    case _:
+                                        msg('You do nothing.')
+                                is_player_turn = False
+                            else:
+                                msg(f'{enemy.name} attacks.')
+                                enemy.attack(plr)
+                                is_player_turn = True
+                        else:
+                            msg('Enemy defeated!', 1)
+                            # TODO: Reward (Gold, XP)
+                            break
+                    else:
+                        break
+                if plr.hp <= 0:
+                    break
             case '2':
                 print('[SHOP]\n')
                 inp = input('> ')
         os.system('cls')
-    
-    print('The End')
+    os.system('cls')
+    print(f"{plr.name}'s adventure is over.\nThe End")
     
 
 if __name__ == '__main__':
