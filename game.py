2ad9c19c4b54dcade9f51bc2dfcefa28609e94c4
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-01 19:07:51 +0200

    Add temporary exec() for debug, new classes and fixes

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Entity:
         self.str = 1
         
     def attack(self, target):
-        dmg = self.str
+        dmg = self.get_dmg()
         target.hp_sub(dmg)
         return dmg
     
@@ -28,6 +28,9 @@ class Entity:
     def hp_sub(self, n):
         self.hp -= n
         
+    def get_dmg(self):
+        return self.str
+        
 class Player(Entity):
     def __init__(self):
         super().__init__()
@@ -38,15 +41,22 @@ class Player(Entity):
         self.lvl = 1
         self.xp = 0
         self.gold = 0
-        self.weapon = None
-        self.armor = None
-        self.bed = None
         self.day = 1
+        self.weapon = Weapon
+        self.armor = Armor
+        self.bed = Bed
+        self.dmg = self.str + self.weapon.dmg
         
-        self.table_xp = {1: 10, 2: 20, 3: 50, 4: 100, 5: 200}
-        self.table_str = {1: 1, 2: 1, 2: 2, 4: 2, 5: 3}
-        self.rest = 5
-        
+        self.table_xp = {1: 10, 2: 20, 3: 30, 4: 40, 5: float('inf')}
+        self.table_str = {1: 1, 2: 1, 3: 2, 4: 2, 5: 3}
+            
+    def buy(self, item, type=None):
+        match type:
+            case 'weapon':
+                self.weapon = item
+            case 'armor':
+                self.armor = item
+    
     def lvl_up(self):
         gain_hp = self.lvl
         gain_str = self.table_str[self.lvl]
@@ -63,9 +73,13 @@ class Player(Entity):
             return True
         else:
             return False
-        
+    
+    def get_dmg(self):
+        return self.str + self.weapon.dmg
+    
     def get_status(self):
-        return f'[{self.name}] - [HP: {self.hp}/{self.hp_max}, STR: {self.str}] - [LVL: {self.lvl}, XP: {self.xp}] - [GOLD: {self.gold}] - [DAY: {self.day}] - [W: {self.weapon} A: {self.armor} BED: {self.bed}]\n'
+        return f'[{self.name}] - [HP: {self.hp}/{self.hp_max}, STR: {self.str}] - [LVL: {self.lvl}, XP: {self.xp}/{self.table_xp[self.lvl]}]'\
+                f' - [GOLD: {self.gold}] - [DAY: {self.day}]\n[W: {self.weapon.name} A: {self.armor.name} BED: {self.bed.name}]\n'
 
 class Enemy(Entity):
     def __init__(self):
@@ -76,6 +90,7 @@ class Enemy(Entity):
         self.str = 1
         self.reward_xp = 1
         self.reward_gold = 1
+        self.dmg = self.str
         
     def get_status(self):
         return f' [{self.name}] - [HP: {self.hp}]\n'
@@ -117,17 +132,17 @@ class Wolf(Enemy):
         self.hp_max = 10
         self.hp = self.hp_max
         self.str = 3
-        self.reward_xp = 15
+        self.reward_xp = 14
         self.reward_gold = 0
         
 class Zombie(Enemy):
     def __init__(self):
         super().__init__()
         self.name = 'Zombie'
-        self.hp_max = 20
+        self.hp_max = 18
         self.hp = self.hp_max
         self.str = 2
-        self.reward_xp = 5
+        self.reward_xp = 12
         self.reward_gold = 8
         
 class Bandit(Enemy):
@@ -137,12 +152,74 @@ class Bandit(Enemy):
         self.hp_max = 20
         self.hp = self.hp_max
         self.str = 4
-        self.reward_xp = 10
+        self.reward_xp = 16
         self.reward_gold = 14
 
-def random_enemy(obj, tier=None):
-    enemies = [Rat, Slime, Goblin]
+class Cobra(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Cobra'
+        self.hp_max = 12
+        self.hp = self.hp_max
+        self.str = 6
+        self.reward_xp = 23
+        self.reward_gold = 0
+        
+class Ghoul(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Ghoul'
+        self.hp_max = 22
+        self.hp = self.hp_max
+        self.str = 5
+        self.reward_xp = 17
+        self.reward_gold = 19
+        
+class Werewolf(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Werewolf'
+        self.hp_max = 30
+        self.hp = self.hp_max
+        self.str = 7
+        self.reward_xp = 32
+        self.reward_gold = 0
+
+class Boss(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Boss'
+        self.hp_max = 100
+        self.hp = self.hp_max
+        self.str = 15
+        self.reward_xp = 0
+        self.reward_gold = 0
+
+class Item():
+    name = None
+    value = 0
+        
+class Weapon(Item):
+    name = None
+    value = 0
+    dmg = 0
+        
+class Club(Weapon):
+    name = 'Club'
+    value = 8
+    dmg = 2
+        
+class Armor(Item):
+    name = None
+    value = 0
+    defense = 0
     
+class Bed(Item):
+    name = None
+    value = 0
+    regen = 5
+
+def random_enemy(obj):
     enemies = {1: [Rat], 
                2: [Rat, Slime],
                3: [Rat, Slime], 
@@ -150,22 +227,38 @@ def random_enemy(obj, tier=None):
                4: [Slime],
                5: [Slime, Goblin],
                6: [Slime, Goblin],
-               6: [Slime, Goblin],
-               7: [Goblin], 
-               8: [Goblin, Wolf],
+               7: [Slime, Goblin],
+               8: [Goblin], 
                9: [Goblin, Wolf],
-               9: [Goblin, Wolf],
-               10: [Wolf], 
-               11: [Wolf, Zombie],
-               12: [Wolf, Zombie],
-               12: [Wolf, Zombie],
-               13: [Zombie],
-               14: [Zombie, Bandit],
-               16: [Zombie, Bandit], 
-               16: [Zombie, Bandit], 
-               17: [Bandit]}
-            
-    result = random.choice(enemies[obj.day])
+               10: [Goblin, Wolf],
+               11: [Goblin, Wolf],
+               12: [Wolf], 
+               13: [Wolf, Zombie],
+               14: [Wolf, Zombie],
+               15: [Wolf, Zombie],
+               16: [Zombie],
+               17: [Zombie, Bandit],
+               18: [Zombie, Bandit], 
+               19: [Zombie, Bandit], 
+               20: [Bandit],
+               21: [Bandit, Cobra],
+               22: [Bandit, Cobra],
+               23: [Bandit, Cobra],
+               24: [Cobra],
+               25: [Cobra, Ghoul],
+               26: [Cobra, Ghoul],
+               27: [Cobra, Ghoul],
+               28: [Ghoul],
+               29: [Ghoul, Werewolf],
+               30: [Ghoul, Werewolf],
+               31: [Ghoul, Werewolf],
+               32: [Werewolf]}
+    
+    try:        
+        result = random.choice(enemies[obj.day])
+    except KeyError:
+        result = Boss
+    
     return result()
 
 def msg(string, duration=1.0):
@@ -174,6 +267,9 @@ def msg(string, duration=1.0):
 
 def main():
     plr = Player()
+    weapons = {1: Weapon, 2: Club}
+    armors = {1: Armor}
+    beds = {1: Bed}
 
     while True:
         print(plr.get_status())
@@ -208,15 +304,12 @@ def main():
                                 msg(f'{enemy.name} attacks for {dmg}.')
                                 is_player_turn = True
                         else:
-                            msg(f'Enemy defeated!\nReward: {enemy.reward_xp} XP | {enemy.reward_gold} Gold')
+                            msg(f'Enemy defeated!\n  Reward: {enemy.reward_xp} XP | {enemy.reward_gold} Gold')
                             plr.xp += enemy.reward_xp
                             plr.gold += enemy.reward_gold
-                            try:
-                                if plr.is_lvl_up_available():
-                                    gain_str = plr.lvl_up()
-                                    msg(f'LEVEL UP!\n  +{gain_str} STR')
-                            except KeyError:
-                                break
+                            while plr.is_lvl_up_available():
+                                gain_str = plr.lvl_up()
+                                msg(f'LEVEL UP!\n  +{gain_str} STR')
                             break
                     else:
                         break
@@ -226,7 +319,7 @@ def main():
                 plr.day += 1
             case '2':
                 rested = False
-                regen = 0
+                total_regen = 0
                 
                 while True:
                     os.system('cls')
@@ -234,16 +327,35 @@ def main():
                     print(' [REST]\n')
                     if not rested:
                         msg('Resting...', 2)
-                        regen = plr.hp_add(plr.rest)
+                        total_regen = plr.hp_add(plr.bed.regen)
                         plr.day += 1
                         rested = True
                     else:
-                        msg(f'Rested for {regen} HP.')
+                        msg(f'Rested for {total_regen} HP.')
                         break
             case '3':
                 print(plr.get_status())
-                print(' [SHOP]\n')
+                print(' [SHOP]\n (1) Weapons\n (2) Armors\n\n (ENTER) Back')
                 inp = input('> ')
+                os.system('cls')
+                match inp:
+                    case '1':
+                        print(plr.get_status())
+                        print(' [SHOP] - [Weapons]\n\n Name / Damage / Price')
+                        for w in weapons:
+                            print(f' ({w}) {weapons[w].name} | {weapons[w].dmg} | {weapons[w].value}')
+                        inp = input('> ')
+                        try:
+                            plr.weapon = weapons[int(inp)]
+                        except KeyError:
+                            pass
+                    case '2':
+                        print(plr.get_status())
+                        print(' [SHOP] - [Armors]\n')
+                        inp = input('> ')
+            case _:
+                exec(inp) # Debug, TO BE REMOVED!
+
         os.system('cls')
     os.system('cls')
     print(f"\n    {plr.name}'s adventure is over.\n\n             The End\n")
